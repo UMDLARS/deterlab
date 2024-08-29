06c2c44812445ea7d41238ca2cc5099e01ca04d3
Eric Nieters <60331611+eric56379@users.noreply.github.com>
2024-08-29 16:14:18 -0500

    Moved around some logic in loadintro.sh to fix error.

--- introjup/intro/resources/loadintro.sh
+++ introjup/intro/resources/loadintro.sh
@@ -14,11 +14,6 @@ mv /tmp/${USER}_intro.tar.gz $TMP
 # Extract the tarball.
 tar -xvf ${TMP}/${USER}_intro.tar.gz
 
-# Check if .inotify_log.txt exists in the temporary directory and copy it if it does.
-if [ -e "${TMP}/inotify_log.txt" ]; then
-    cp "${TMP}/inotify_log.txt" "/home/.checker/inotify_log.txt"
-fi
-
 # Check if findme.txt exists in the temporary directory and copy it if it does.
 if [ -e "${TMP}/findme.txt" ]; then
     cp "${TMP}/findme.txt" "/usr/share/discover/dtd/findme.txt"
@@ -27,6 +22,11 @@ fi
 # Check if the home directory exists (which it should), then copy it if it does.
 if [ -e "${TMP}/${USER}" ]; then
     cp -r ${TMP}/${USER}/* ~
+fi
+
+# Check if .inotify_log.txt exists in the temporary directory and copy it if it does.
+if [ -e "${TMP}/inotify_log.txt" ]; then
+    cp "${TMP}/inotify_log.txt" "/home/.checker/inotify_log.txt"
 fi
 
 # Return to the previous directory.
