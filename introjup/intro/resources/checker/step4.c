76ec8e5537fc8b3da0ef03d864da7d4c914afa88
Eric Nieters <60331611+eric56379@users.noreply.github.com>
2024-08-29 19:36:19 -0500

    Update step4.c

--- introjup/intro/resources/checker/step4.c
+++ introjup/intro/resources/checker/step4.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 int main() {
-    const char *hidden_file = "/usr/share/discover/dtd/findme.txt";
+    const char *hidden_file = "/usr/share/discover/dtd/.findme.txt";
     if (access(hidden_file, F_OK) == 0) {
         return 1;
     }
