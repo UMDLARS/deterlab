ec52490840248d5d56668f04f9006b42eeeb7662
Eric Nieters <eric56379@gmail.com>
2024-10-08 19:58:35 -0500

    Fixes to intro.

--- introjup/intro/resources/checker/run_inotify.sh
+++ introjup/intro/resources/checker/run_inotify.sh
@@ -2,4 +2,4 @@
 
 # Create a new, detached screen session with the inotifywait session to recursively track the home directory.
 
-screen -dmS inotify_session bash -c "inotifywait -m -r -e create -e delete -e attrib ~ > /home/.checker/inotify_log.txt"
+screen -dmS inotify_session bash -c "inotifywait -m -r -e create -e delete -e attrib ~ >> /home/.checker/inotify_log.txt"
