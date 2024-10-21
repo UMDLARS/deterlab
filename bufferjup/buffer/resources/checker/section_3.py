87cc38aa4c996ff36ec2b891d0b0b0372f197c81
Eric Nieters <eric56379@gmail.com>
2024-10-21 15:11:13 -0500

    Logic issues with buffer that were fixed.

--- bufferjup/buffer/resources/checker/section_3.py
+++ bufferjup/buffer/resources/checker/section_3.py
@@ -44,7 +44,7 @@ def main():
     step = sys.argv[1]
 
     # We need the student's username throughout this entire lab.
-    username = "USERNAME_GOES_HERE"
+    username = "USERNAME_FOR_NODE"
 
     vulnerable = "/home/" + username + "/topic_2/step_" + str(int(step) - 4) + ".c"
     fix = "/home/" + username + "/topic_3/step_" + step + ".c"
