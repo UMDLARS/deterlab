5ee0129832562c2ca8464a34fe1bd5770eb676ee
Eric Nieters <60331611+eric56379@users.noreply.github.com>
2025-02-04 17:40:32 -0600

    Changes to save.py for more stability.

--- notebooks/resources/save.py
+++ notebooks/resources/save.py
@@ -7,51 +7,49 @@ import time
 def main():
     labname = sys.argv[1]
 
-    # Remove the existing save so that the new one can be made.
-    process = subprocess.run('rm -f /project/USERNAME_GOES_HERE/notebooks/saves/USERNAME_GOES_HERE_' + labname + '.tar.gz', shell=True, stdout=subprocess.DEVNULL)
-    print("File removed from ~/notebooks/saves.")
+    # (No longer remove the existing file.)
 
-    # Generate the backup. XSS has two nodes, so needs a slightly different statement.
-    if (labname == "xss"):
-        process = subprocess.run("ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@server '/home/.checker/save" + labname + ".sh'", shell=True, stdout=subprocess.DEVNULL)
-
-    # The firewalls lab needs a special SSH statement, since it relies on environment variables.
-    elif (labname == "firewalls"):
-        process = subprocess.run('ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@server "bash -l -c \'/home/.checker/savefirewalls.sh\'"', shell=True, stdout=subprocess.DEVNULL)
-
-    # Everything else can be saved the same way.
+    # Generate the backup via SSH as before.
+    if labname == "xss":
+        subprocess.run("ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@server '/home/.checker/save" + labname + ".sh'",
+                       shell=True, stdout=subprocess.DEVNULL)
+    elif labname == "firewalls":
+        subprocess.run('ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@server "bash -l -c \'/home/.checker/savefirewalls.sh\'"',
+                       shell=True, stdout=subprocess.DEVNULL)
     else:
-        process = subprocess.run("ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@" + labname + " '/home/.checker/save" + labname + ".sh'", shell=True, stdout=subprocess.DEVNULL)
-        
+        subprocess.run("ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@" + labname + " '/home/.checker/save" + labname + ".sh'",
+                       shell=True, stdout=subprocess.DEVNULL)
     print("Backup tarball created.")
 
+    # Define temporary and final filenames.
+    dest_dir = "/project/USERNAME_GOES_HERE/notebooks/saves"
+    final_filename = os.path.join(dest_dir, "USERNAME_GOES_HERE_" + labname + ".tar.gz")
+    temp_filename  = final_filename + ".new"
 
-    
-    # Get the backup into the XDC, then transfer it.
-    if (labname == "xss" or labname == "firewalls"):
-        process = subprocess.run("scp -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@server:~/USERNAME_GOES_HERE_" + labname + ".tar.gz /project/USERNAME_GOES_HERE/notebooks/saves &> /dev/null", shell=True, stdout=subprocess.DEVNULL)
-
+    # Copy the backup from the remote server using scp to a temporary file.
+    if labname in ["xss", "firewalls"]:
+        scp_cmd = "scp -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@server:~/" + os.path.basename(final_filename) + " " + temp_filename + " &> /dev/null"
     else:
-        process = subprocess.run("scp -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@" + labname + ":~/USERNAME_GOES_HERE_" + labname + ".tar.gz /project/USERNAME_GOES_HERE/notebooks/saves &> /dev/null", shell=True, stdout=subprocess.DEVNULL)
-        
-    print("Copied onto the XDC.")
+        scp_cmd = "scp -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@" + labname + ":~/" + os.path.basename(final_filename) + " " + temp_filename + " &> /dev/null"
+    subprocess.run(scp_cmd, shell=True, stdout=subprocess.DEVNULL)
+    print("Backup tarball copied onto the XDC as a temporary file.")
 
-
-    
-    # Wait until the file is FULLY transferred before deleting remotely.
-    # This is a race condition that happens due to bandwidth.
-    while not os.path.exists("/project/USERNAME_GOES_HERE/notebooks/saves/USERNAME_GOES_HERE_" + labname + ".tar.gz"):
+    # Wait until the file has been transferred.
+    while not os.path.exists(temp_filename):
         time.sleep(1)
 
+    # Atomically replace (or create) the final file by renaming the temp file.
+    os.rename(temp_filename, final_filename)
+    print("New tarball has been moved into place.")
 
-    
-    # Delete the lab's tarball remotely.
-    if (labname == "xss" or labname == "firewalls"):
-        process = subprocess.run("ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@server 'rm -f USERNAME_GOES_HERE_" + labname + ".tar.gz &> /dev/null'", shell=True, stdout=subprocess.DEVNULL)
-
+    # Delete the lab's tarball on the remote system.
+    if labname in ["xss", "firewalls"]:
+        subprocess.run("ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@server 'rm -f " + os.path.basename(final_filename) + " &> /dev/null'",
+                       shell=True, stdout=subprocess.DEVNULL)
     else:
-        process = subprocess.run("ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@" + labname + " 'rm -f USERNAME_GOES_HERE_" + labname + ".tar.gz &> /dev/null'", shell=True, stdout=subprocess.DEVNULL)
+        subprocess.run("ssh -i /home/USERNAME_GOES_HERE/.ssh/merge_key USERNAME_GOES_HERE@" + labname + " 'rm -f " + os.path.basename(final_filename) + " &> /dev/null'",
+                       shell=True, stdout=subprocess.DEVNULL)
+    print("Remote tarball removed.")
 
-    print("File removed remotely.")
-
-main()
+if __name__ == "__main__":
+    main()
